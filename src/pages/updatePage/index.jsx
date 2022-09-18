fc04890b5706a5c6a8057da61aedfe8fb8d8ca9c
Dec <dec@GentleStorms-Air.local>
2022-09-18 06:33:14 +0100

    setup bug fix and update profile boilerplate

---
+++ src/pages/updatePage/index.jsx
@@ -0,0 +1,16 @@
+import "./index.scss";
+import Nav from "../../components/nav/";
+
+function UpdatePage(){
+    
+    return (
+        <>
+            <Nav />
+            <div className="top-bg">
+
+            </div>
+        </>
+    )
+}
+
+export default UpdatePage
