548721b355beab40c5a7c1d3004dd9784c6f1ab9
Eten-Ekata <ekataelishaeten@gmail.com>
2022-09-20 02:46:51 +0100

    Updated UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { BrowserRouter, Routes, Route } from "react-router-dom";
 // components
-import Auth from './components/auth';
+import Auth from "./components/auth";
 // pages
 import LandingPage from "./pages/landingPage";
-import LoginPage from './pages/login';
+import LoginPage from "./pages/login";
 import SignupPage from "./pages/signup";
 import PageNotFound from "./pages/notfound";
 import UpdatePage from "./pages/updateUser";
-
 
 function App() {
   return (
@@ -19,6 +18,9 @@ function App() {
         <Route path="/register" element={<SignupPage />}></Route>
         <Route path="/update" element={<UpdatePage />}></Route>
         <Route path="*" element={<PageNotFound />} />
+        <Route path="/signupPage" element={<SignupPage />}>
+          {" "}
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/signup/Index.jsx
+++ src/pages/signup/Index.jsx
@@ -1,13 +1,75 @@
-import React from 'react';
-import './style.scss'
+import React from "react";
+import "./style.scss";
+import logo from "../../assets/icons/logo.svg";
 
 const Signup = () => {
-    return (
-        <div>
-            <h1>This is signup page</h1>
-        </div>
-    );
+  return (
+    <div className="container">
+      <img src={logo} alt="logo" />
 
+      <div className="account">
+        <button className="back-btn">
+          {" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="heroicon"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+            />
+          </svg>
+          Go back
+        </button>
+        <div className="text">
+          <h2>Create an account</h2>
+        </div>
+        <form>
+          <div className="blocks">
+            <h4 className="eten">Name</h4>
+            <input type="text" placeholder="Enter your name" className="help" />
+          </div>
+          <div className="blocks">
+            <h4 className="eten">Email</h4>
+            <input
+              type="text"
+              placeholder="Enter your email"
+              className="help"
+            />
+          </div>
+          <div className="blocks">
+            <h4 className="eten-phone">Phone Number</h4>
+            <input
+              type="text"
+              placeholder="Enter your phone number"
+              className="help"
+            />
+          </div>
+          <div className="blocks">
+            <h4 className="eten">Password</h4>
+            <input
+              type="text"
+              placeholder="Enter your password"
+              className="help"
+            />
+          </div>
+          <div className="blocks">
+            <h4 className="eten-password">Confirm Password</h4>
+            <input
+              type="text"
+              placeholder="Confirm Password"
+              className="help"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
