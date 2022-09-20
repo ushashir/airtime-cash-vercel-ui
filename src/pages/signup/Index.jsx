ca13bc1f9a7b1b3602964428c12695dcc41b54c9
Eten-Ekata <ekataelishaeten@gmail.com>
2022-09-20 19:27:22 +0100

    Completed use states for all input fields

--- src/pages/signup/Index.jsx
+++ src/pages/signup/Index.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
 import React from "react";
 import "./style.scss";
 import logo from "../../assets/icons/logo.svg";
 
 const Signup = () => {
+  const [name, setName] = useState("");
+  const [mail, setMail] = useState("");
+  const [number, setNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [passconfirm, setPassconfirm] = useState("");
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handleMail = (e) => {
+    setMail(e.target.value);
+  };
+  const handlePhone = (e) => {
+    setNumber(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleConfirm = (e) => {
+    setPassconfirm(e.target.value);
+  };
   return (
     <div className="container">
       <img src={logo} alt="logo" />
@@ -32,40 +53,59 @@ const Signup = () => {
         <form>
           <div className="blocks">
             <h4 className="eten">Name</h4>
-            <input type="text" placeholder="Enter your name" className="help" />
+            <input
+              onChange={handleName}
+              type="text"
+              placeholder="Enter your name"
+              className="help"
+              value={name}
+            />
           </div>
           <div className="blocks">
             <h4 className="eten">Email</h4>
             <input
+              onChange={handleMail}
               type="text"
               placeholder="Enter your email"
               className="help"
+              value={mail}
             />
           </div>
           <div className="blocks">
             <h4 className="eten-phone">Phone Number</h4>
             <input
+              onChange={handlePhone}
               type="text"
               placeholder="Enter your phone number"
               className="help"
+              value={number}
             />
           </div>
           <div className="blocks">
             <h4 className="eten">Password</h4>
             <input
+              onChange={handlePassword}
               type="text"
               placeholder="Enter your password"
               className="help"
+              value={password}
             />
           </div>
           <div className="blocks">
             <h4 className="eten-password">Confirm Password</h4>
             <input
+              onChange={handleConfirm}
               type="text"
               placeholder="Confirm Password"
               className="help"
+              value={passconfirm}
             />
           </div>
+          <button className="eten-sign"> Sign Up</button>
+          <h4 className="kiki">
+            <span className="dolly">Already have an account?</span>
+            <span className="clem">Sign In</span>
+          </h4>
         </form>
       </div>
     </div>
