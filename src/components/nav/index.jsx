fd0fb2a10908307316bbdd975a432194a16beb0c
Eten-Ekata <ekataelishaeten@gmail.com>
2022-09-21 02:12:22 +0100

    updated

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -4,7 +4,7 @@ import logo from "../../assets/icons/logo.svg";
 function Nav() {
   return (
     <>
-      <nav className="admin-navbar">
+      <nav className="navbar">
         <div className="logo-image">
           <img src={logo} alt="airtime to cash logo"></img>
         </div>
