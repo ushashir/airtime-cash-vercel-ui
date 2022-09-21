a76b32219afbe6fff2e130d69019ad308c615517
Eten-Ekata <ekataelishaeten@gmail.com>
2022-09-21 08:35:33 +0100

    resolve index conflict

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,18 +1,18 @@
 import "./style.scss";
-import logo from "../../../assets/icons/logo.svg";
-import avatar from "../../../assets/images/dashboard_avata.png";
+import logo from "../../assets/icons/logo.svg";
 
 function Nav() {
   return (
-    <div className="nav-bar">
-      <div>
-        <img src={logo} alt="airtime to cash logo"></img>
-      </div>
-      <div className="user-bar">
-        <img src={avatar} alt="user avatar" />
-        <p>Ushahemba</p>
-      </div>
-    </div>
+    <>
+      <nav className="navbar">
+        <div className="logo-image">
+          <img src={logo} alt="airtime to cash logo"></img>
+        </div>
+        <div>
+          <h4>Photo</h4>
+        </div>
+      </nav>
+    </>
   );
 }
 
