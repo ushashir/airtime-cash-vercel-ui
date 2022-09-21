c06d6d07ffd66406c63e5cf864948b16aadffe37
profCodey <otunaiyademilade@gmail.com>
2022-09-21 14:02:23 +0100

    updated landing page

--- src/components/advert/index.jsx
+++ src/components/advert/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './style.scss'
 import phonetouch from "../../assets/phonetouch.png";
+import Button from "../common/button";
 
 
 export default function Adevert() {
@@ -13,10 +14,20 @@ export default function Adevert() {
               <div className="advert_text">
                 <h2>The best platform for your convenient airtime exchange</h2>
                 <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum
-                  praesentium dolorum necessitatibus libero, quas reprehenderit.
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Illum praesentium dolorum necessitatibus libero, quas
+                  reprehenderit.
                 </p>
-                <div className="my-btn">Get Started</div>
+                <Button
+                  value="Get Started"
+                  style={{
+                    fontSize: "1.6rem",
+                    textAlign: "center",
+                    textDecoration: "none",
+                    borderRadius: "24px",
+                    marginLeft:  "9rem",
+                  }}
+                />
               </div>
             </div>
           </div>
