1f4b8fb6a95ccc7e17e457dffcfc6dd86097f257
Oforkaja Joseph <oforkajajoseph@gmail.com>
2022-09-23 09:27:55 +0100

    created transactionHistory component

---
+++ src/pages/transactionHistory/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+async function TransactionHistory() {
+
+    return (
+        <div className='tH-Frame-8747'>
+            <div className='tH-Frame-8744'>
+                <div className='tH-Frame-8743'>
+                    <p>Today, 10:15AM</p>
+                    <p>Withdraw fund</p>
+                    <p>25/5/2022</p>
+                </div>
+                <div className='tH-Frame-8650'>
+                    <div className='tH-Frame-8537'>
+                        <p>Received</p>
+                    </div>
+                    <p>N5,000</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default TransactionHistory;
