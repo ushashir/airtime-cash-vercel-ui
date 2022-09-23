990a4c7c2b98e1e452ce4d6eb4ed786186ca1447
Decagon <decagon@Decagons-MacBook-Air-Usha.local>
2022-09-23 22:18:26 +0100

    withdraw balance style update

--- src/components/withdrawBalance/index.jsx
+++ src/components/withdrawBalance/index.jsx
@@ -1,42 +1,85 @@
-
-
-import { useState } from 'react';
 import './style.scss'
+// import React from 'react';
+import React, { useState } from 'react';
 
-// inititaal states
-// const [selectAccount, setSelectAccount] = useState("");
-// const [accountName, setAccountName] = useState("");
-// const [accountName, setAccountNumber] = useState("");
-// const [amount, setAmount] = useState("");
-// const [password, setPassword] = useState("");
- 
+
+
 function Withdraw() {
+    const initialState = {
+    selectAccount: '',
+    accountName: '',
+    accountNumber: '',
+    amount: '',
+    password: '',
+  }
+  const [withdraw, setWithdraw] = useState(initialState)
+const { selectAccount, accountName, accountNumber, amount, password } = withdraw;
+
+  const onChange = e => setWithdraw({...withdraw, [e.target.name]: e.target.value})
+
+  const onSubmit = e => {
+    e.preventDefault()
+    if (
+      selectAccount === '' ||
+      accountName === '' ||
+      accountNumber === '' ||
+      amount === '' ||
+      password === '' 
+    ) {
+      console.log("enter all fields")
+    } else {
+        console.log("call register function register")
+    //   register({selectAccount, accountName, accountNumber, password})
+    }
+  }
+ 
     return (
         <div className='withdraw-balance'>
             <p>Withdraw </p>
             <form action="">
-                <div className=' input-field'>
+                <div className='input-field'>
                     <label htmlFor="selectAccount">Select Account</label> <br></br>
                     <input type="text"
                         name='selectAccount'
                         placeholder='select'
+                        value={selectAccount}
+                        onChange={onChange}
                        />
                 </div>
                  <div className=' input-field'>
                     <label htmlFor="accountName">Account Name</label> <br></br>
-                    <input type="text" name='accountName' placeholder='BabatundeOla' />
+                    <input type="text"
+                        name='accountName'
+                        placeholder='BabatundeOla'
+                        value={accountName}
+                        onChange={onChange}
+                    />
                 </div>
                  <div className=' input-field'>
                     <label htmlFor="accountNumber">Account Number</label> <br></br>
-                    <input type="number" name='accountNumber' placeholder='8746526671' />
+                    <input type="number"
+                        name='accountNumber'
+                        placeholder='8746526671'
+                        value={accountNumber}
+                        onChange={onChange}/>
                 </div>
                  <div className=' input-field'>
                     <label htmlFor="amount">Amount</label> <br></br>
-                    <input type="number" name='amount' placeholder='NGN' />
+                    <input type="number"
+                        name='amount'
+                        placeholder='NGN'
+                        value={amount}
+                        onChange={onChange}
+                    />
                 </div>
                  <div className=' input-field'>
                     <label htmlFor="password">Password</label> <br></br>
-                    <input type="number" name='password' placeholder='password' />
+                    <input type="number"
+                        name='password'
+                        placeholder='password'
+                         value={password}
+                        onChange={onChange}
+                    />
                 </div>
                <button>Withdraw</button>
                
