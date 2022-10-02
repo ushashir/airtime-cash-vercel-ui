4f214a5d2545d378b974c0295914b04899d4c311
nells30 <chinelo.olikeze@decagon.dev>
2022-10-02 16:47:05 +0100

    pull req

--- src/components/withdrawalHistory/index.jsx
+++ src/components/withdrawalHistory/index.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import './style.scss'
 
-const TransactionHistory = () => {
+const WithdrawalHistory = () => {
 
     return (
-        <div className='tH-Frame1-8747'>
-            <div className='tH-Frame2-8744'>
-                <div className='tH-Frame3-8743'>
-                    <p className='tH-day_time1'><b>Status:</b></p>
-                    <p className='tH-withdraw2'><b>Airtime Amount:</b></p>
-                    <p className='tH-withdraw2'><b>Network:</b></p>
-                    <p className='tH-date3'><b>Date of Transaction:</b></p>
+        <div className='tH-Frame-8747'>
+            <div className='tH-Frame-8744'>
+                <div className='tH-Frame-8743'>
+                    <p className='tH-day_time'><b>Today,</b> 10:15AM</p>
+                    <p className='tH-withdraw'>Withdraw fund</p>
+                    <p className='tH-date'>25/5/2022</p>
                 </div>
-                <div className='tH-Frame4-8650'>
-                        <p className='tH-received1'>Received</p>
-                        
-                 </div>
-             </div>
+                <div className='tH-Frame-8650'>
+                    <div className='tH-Frame-8537'>
+                        <p className='tH-received'>Received</p>
+                    </div>
+                    <p>N5,000</p>
+                </div>
+            </div>
 
-{/* 
+
             <div className='tH-Frame-8744'>
                 <div className='tH-Frame-8743'>
                     <p className='tH-day_time'><b>Today,</b> 10:15AM</p>
@@ -58,9 +59,9 @@ const TransactionHistory = () => {
                     </div>
                     <p>N5,000</p>
                 </div>
-            </div> */}
+            </div>
         </div>
     );
 }
 
-export default TransactionHistory;
+export default WithdrawalHistory;
