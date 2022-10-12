0518ad2b63aa7c5524d2c974e92668701d7a2e84
Decagon <decagon@Decagons-MacBook-Air-Usha.local>
2022-10-12 15:33:03 +0100

    integration bug removed

--- src/__tests__/register.test.js
+++ src/__tests__/register.test.js
@@ -1,8 +1,16 @@
 function sum(a, b) {
   return a + b;
 }
+function subtract(a, b) {
+  return a - b;
+}
 export default sum;
 
 test("test sum of two numbers", () => {
   expect(sum(4, 5)).toEqual(9);
 });
+
+test("test substration of two numbers", () => {
+  expect(subtract(10, 5)).toEqual(5);
+});
+
