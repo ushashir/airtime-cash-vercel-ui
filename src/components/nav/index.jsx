9b14463d124b5b55b694a1a6aad686e7620aff8b
nells30 <chinelo.olikeze@decagon.dev>
2022-10-12 22:30:27 +0100

    Docker for FE

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,17 +1,63 @@
 import "./style.scss";
 import logo from "../../assets/icons/logo.svg";
+import logoicon from "../../assets/icons/logo_icon.svg";
+import { useContext, useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../../context/userContext";
+import UserProfileDropdown from "../dropdown";
+import dropDownIcon from "../../assets/icons/dropdownicon.svg";
 
 function Nav() {
+  const { user } = useContext(UserContext);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const ref = useRef(null);
+
+  let imageAvatar;
+  !user
+    ? imageAvatar = 'https://www.nicepng.com/png/detail/115-1150821_default-avatar-comments-sign-in-icon-png.png'
+    : imageAvatar = user.avatar;
+  const handleClickOutside = (event) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+
+      setShowDropdown(false)
+
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, true);
+  });
   return (
     <>
-      <nav className="navbar">
-        <div className="logo-image">
-          <img src={logo} alt="airtime to cash logo"></img>
+      <div className="nav">
+        <div className="nav-bar">
+          <div>
+            <Link to={"/dashboard"}>
+              <picture>
+                <source srcSet={logoicon} media="(max-width: 500px)" />
+                <img src={logo} alt="airtime to cash logo" style={{ padding: "10px" }} />
+              </picture>
+            </Link>
+          </div>
+          <div className="user-bar">
+            <span
+              style={{
+                display: "flex",
+                gap: "2px",
+
+                alignItems: "center",
+              }}
+              ref={ref} onClick={() => setShowDropdown(prev => !prev)}>
+              <img src={imageAvatar} alt="user avatar" className="avatar" />
+              <p>{user && user.userName}</p>
+              <img
+                style={{ width: "18px", paddingTop: "5px" }}
+                src={dropDownIcon}
+              />
+            </span>
+          </div>
+          {showDropdown && <UserProfileDropdown showDropdown={showDropdown} />}
         </div>
-        <div>
-          <h4>Photo</h4>
-        </div>
-      </nav>
+      </div>
     </>
   );
 }
